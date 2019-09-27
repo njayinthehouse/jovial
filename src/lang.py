45999de3307103f2a4b624163472c26f6aa9b7ac
Nitin John Raj <nitinjohnraj@pal-nat186-67-75.itap.purdue.edu>
2019-09-26 21:43:13 -0400

    Refactored some functionality from Backend to State

--- src/lang.py
+++ src/lang.py
@@ -1,3 +1,4 @@
+# pyre-strict
 from abc    import ABC, abstractmethod
 from typing import Callable, Dict, Generic, List, NamedTuple, TypeVar
 
@@ -28,38 +29,61 @@ class Frontend(ABC, Generic[Branch]):
         msg: str
         update: Update
 
-    def parseCommand(self, statement: str) -> Command:
-        pass
+    arity = {"merge": 2, "fork": 2, "commit": 4}
+
+    def parseCommand(self, command: str) -> Command:
+        args = command.split(" ")
+        c = args[0]
+
+        try:
+            if c == "merge":
+                return self.Merge(*args[1:])
+            elif c == "fork":
+                return self.Fork(*args[1:])
+            elif c == "commit":
+                tail = "".join(args[3:]).split("\"")[1:]
+                msg = tail[0]
+                update = self.parseUpdate(tail[1])
+                return self.Commit(args[1], args[2], msg, update)
+            else:
+                raise Exception("Parse error: Unknown command %s" % args[0])
+        except TypeError:
+            raise Exception("Parse error: Arity error for command %s. %d arguments were expected, %d were given." % (c, self.arity[c], len(args[1:])))
+        except IndexError:
+            raise Exception("Parse error: Arity error for command %s. %d arguments were expected, %d were given." % (c, self.arity[c], len(args[1:])))
 
     @abstractmethod
-    def parseUpdate(self, update: str):
+    def parseUpdate(self, update: str) -> Update:
         pass
+
+    def parse(self, program: List[str]) -> List[Command]:
+        return list(map(self.parseCommand, program))
 
 class State(ABC, Generic[Branch, Local]):
     @abstractmethod
     def get(self, br: Branch) -> Local: pass
 
     @abstractmethod 
-    def set(self, br: Branch, local: Local): pass
+    def set(self, br: Branch, local: Local) -> State[Branch, Local]: pass
     
     @abstractmethod
-    def common_ancestor(self, br1: Branch, br2: Branch): pass
+    def common_ancestor(self, br1: Branch, br2: Branch) -> Branch: pass
     
     @abstractmethod
-    def merge(self, br1: Branch, br2: Branch): pass
+    def merge(self, br1: Branch, br2: Branch) -> State[Branch, Local]: pass
     
     @abstractmethod
-    def fork(self, br: Branch, brn: Branch): pass
+    def fork(self, br: Branch, brn: Branch) -> State[Branch, Local]: pass
     
     @abstractmethod
-    def commit(self, br: Branch, cid: str, msg: str, update: Callable[[Local], Local]): pass
+    def commit(self, br: Branch, cid: str, msg: str, update: Callable[[Local], Local]) -> State[Branch, Local]: pass
     
 class Backend(ABC, Generic[Branch, Local]):
 
-    def run(self, state: State[Branch, Local], program: List[Frontend.Command]):
+    def run(self, state: State[Branch, Local], program: List[Frontend.Command]) -> None:
         for command in program:
-            if self.alert():
-                self.record_state()
+            if self.alert(state):
+                self.record_state(state)
                 break
             else:
                 self.eval(state, command)
@@ -72,22 +96,22 @@ class Backend(ABC, Generic[Branch, Local]):
             br, brn = command                               # pyre-ignore
             return state.fork(br, brn)
         elif type(command) == Frontend.Commit:
-            br, cid, msg, update = command                  # pyre-ignore
-            return state.commit(br, cid, msg, update)
+            br, cid, msg, updateNode = command                  # pyre-ignore
+            return state.commit(br, cid, msg, self.update(updateNode))
         else:
             raise Exception("Command not found!")
 
     @abstractmethod
-    def alert(self, state) -> bool:
+    def alert(self, state: State[Branch, Local]) -> bool:
         '''For cases where we want to break execution and store the result'''
         pass
 
     @abstractmethod
-    def record_state(self, state):
+    def record_state(self, state: State[Branch, Local]) -> None:
         '''To record interesting situations'''
         pass
     
     @abstractmethod
-    def update(self, update: Frontend[Branch].Update): 
+    def update(self, update: Frontend.Update) -> Callable[[LocalState], LocalState]: 
         '''To update the local state. Implements the Update AST node.'''
  
