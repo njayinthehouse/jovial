955756e28a8377618f0a1c438ceec95d7f19c154
njayinthehouse <nitinjohnraj@gmail.com>
2019-11-10 13:42:02 -0500

    Type annotations in pursuit

--- src/pursuit.py
+++ src/pursuit.py
@@ -1,15 +1,16 @@
+# pyre-strict
 from abc import ABC
 
 class Action(ABC): pass
 
 class Insert(Action):
-    def __init__(self, i, br):
+    def __init__(self, i: int, br: str):
         self.i, self.br = i, br
 
 class Replace(Action):
-    def __init__(self, i, br):
+    def __init__(self, i: int, br: str):
         self.i, self.br = i, br
 
 class Merge(Action):
-    def __init__(self, br1, br2):
+    def __init__(self, br1: str, br2: str):
         self.br1, self.br2 = br1, br2
